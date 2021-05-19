5f265a7068d6834a173fa9e87f9f176e6b57daff
Avivit Weissman <wavivit@gmail.com>
2021-05-19 19:47:23 +0000

    add creatTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,28 +12,32 @@ $(document).ready(function() {
 
 
 
-// const tweetObj = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+const tweetObj = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
 
-// const createTweetElement = function(tweetObj){
+const createTweetElement = function(tweetObj){
 
-//   let tweetHtml = `
-//   <div>
-//   <span>${tweetObj.user.name}</span>
-//   </div>
-//   `;
-//   const $tweet = $(tweetHtml);
+  let $tweet =   $(`<article class="tweet">Hello world 
+                  <header><span class="name">Vivi theBest</span><span class="handle">@vivi</span></header>
+                  <div>this is the main part: tweet tweet tweet</div>
+                  <footer><span>10 Days ago</span><i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></footer>
+                  </article>`);
 
-//   return $tweet;
-// }
+  return $tweet;
 
-// $('.tweets-list').append(createTweetElement(tweetObj));
+}
+
+// const $tweet = createTweetElement(tweetObj);
+
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('.tweets--list').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
