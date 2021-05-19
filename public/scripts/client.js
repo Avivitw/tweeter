748664becd6090a3af319bc8ba5b139444661344
Avivit Weissman <wavivit@gmail.com>
2021-05-19 06:03:05 +0000

    added tweets section and styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,36 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $(".tweet footer span").text(timeago.format(1621229942803));
+});
+
+
+
+
+
+// const tweetObj = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+// const createTweetElement = function(tweetObj){
+
+//   let tweetHtml = `
+//   <div>
+//   <span>${tweetObj.user.name}</span>
+//   </div>
+//   `;
+//   const $tweet = $(tweetHtml);
+
+//   return $tweet;
+// }
+
+// $('.tweets-list').append(createTweetElement(tweetObj));
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(`#tweet-text`).on(`input`, function(){
+    let tweetLength= $(this).val().length;
+    let $counter = $(this).siblings(`.new-tweet-bottom`).first().find(`.counter`).text(140 - tweetLength);
+    $counter.toggleClass(`invalid`, tweetLength > 140);
+
+  })
+  console.log("were ready")
+});
